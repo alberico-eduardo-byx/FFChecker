df9d20c47ecd88fc069ee95c1c31ca9ca8855823
Alberico Soares <alberico.soares@grupobyx.com.br>
2025-08-12 11:26:01 -0300

    feat: Add initial app structure with models, views, and admin registration

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+# Create your views here.
+class FeatureViewAPIView(APIView):
+    def post(self, request):
+        return Response(
+            {'message': 'API Funcionando'},
+            status=status.HTTP_200_OK,
+        )
